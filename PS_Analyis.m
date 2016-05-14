502eaf08e672122a993474e2485f66e8891024ae
fedecarne <fedecarne@gmail.com>
2016-05-14 18:14:30 -0400

    update protocol

--- PS_Analyis.m
+++ PS_Analyis.m
@@ -1,3 +1,4 @@
+
 TrialTypes = SessionData.TrialTypes-1; %0: rewarded port 1, %1: rewarded port 3
 nTrial=size(TrialTypes,2);
 
@@ -8,16 +9,29 @@ Side = nan(1,nTrial); %0:left, 1:right
 Side(Outcomes==1 | Outcomes==2)=TrialTypes(Outcomes==1 | Outcomes==2);
 Side(Outcomes==0) = ~TrialTypes(Outcomes==0);
 
-% Fraction left vs distance to switch
-
+% Fraction left vs distance to left right switch
 FirstBlockChange = [0 diff(TrialTypes)];
-ind_FirstBlockChange= find(FirstBlockChange);
-t_p=2;t_f=2;
-if nTrial-ind_FirstBlockChange(end)<t_f;
-    Side=[Side nan(1,nTrial-ind_FirstBlockChange(end)<t_f)];
+
+ind_LR_BlockChange= find(FirstBlockChange==-1);
+ind_RL_BlockChange= find(FirstBlockChange==1);
+
+t_p=9;t_f=9;
+if nTrial-ind_LR_BlockChange(end)<t_f
+    Side=[Side nan(1,nTrial-ind_LR_BlockChange(end)+1)];
+end
+if nTrial-ind_RL_BlockChange(end)<t_f
+    Side=[Side nan(1,nTrial-ind_RL_BlockChange(end)+1)];
 end
 
+pLR=nan(1,t_p+t_f+1);%fraction of right
+pRL=nan(1,t_p+t_f+1);%fraction of right
 for i=-t_p:+t_f
-    p(i+t_p+1) = nanmean(Side(find(FirstBlockChange)+i));
+    pLR(i+t_p+1) = nanmean(Side(ind_LR_BlockChange+i));
+    pRL(i+t_p+1) = nanmean(Side(ind_RL_BlockChange+i));
 end
 
+figure
+plot(-t_p:t_f,1-pLR);hold on
+plot(-t_p:t_f,1-pRL)
+ylabel('fraction of left')
+legend('L-R','R-L'); legend boxoff
